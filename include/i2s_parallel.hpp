954c13cb0e5a233daf5ba622b9163737ddf7d17f
Martin Mueller <MartinMueller2003@yahoo.com>
2026-08-30 20:37:40 -0400

    Snapshot non working version

--- include/i2s_parallel.hpp
+++ include/i2s_parallel.hpp
@@ -5,24 +5,43 @@
 
 #include <freertos/FreeRTOS.h>
 #include <driver/i2s.h>
+
 #include <esp_err.h>
 #include <rom/lldesc.h>
+// #include "hal/i2s_hal.h"
 
-#define I2S_PARALLEL_CLOCK_HZ 80000000L
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-typedef enum {
+// #define I2S_PARALLEL_CLOCK_HZ 80000000L
+#define I2S_PARALLEL_CLOCK_HZ 160000000L
+
+enum I2sParallelDmaBufferOwner
+{
+  I2sParallelDmaBufferOwnerDMA = 0,
+  I2sParallelDmaBufferOwnerCPU = 1,
+};
+
+typedef enum
+{
   I2S_PARALLEL_WIDTH_8,
   I2S_PARALLEL_WIDTH_16,
   I2S_PARALLEL_WIDTH_24,
   I2S_PARALLEL_WIDTH_MAX
 } i2s_parallel_sample_width_t;
 
-typedef struct {
+typedef struct
+{
   int                         gpio_clk;
   int                         gpios_bus[24]; // The parallel GPIOs to use, set gpio to -1 to disable
   i2s_parallel_sample_width_t sample_width;
   int                         sample_rate;
 } i2s_parallel_config_t;
+
+#ifndef I2S_PIN_NO_CHANGE
+#define I2S_PIN_NO_CHANGE gpio_num_t(-1)
+#endif // ndef I2S_PIN_NO_CHANGE
 
 void i2s_parallel_iomux_set_signal(int gpio, int signal);
 
@@ -32,11 +51,15 @@ esp_err_t i2s_parallel_send_dma(i2s_port_t port, lldesc_t* dma_descriptor);
 
 i2s_dev_t* i2s_parallel_get_dev(i2s_port_t port);
 
+double i2s_parallel_driver_get_clockNS();
+
 static inline int i2s_parallel_get_memory_width(i2s_port_t port, i2s_parallel_sample_width_t width) {
-  switch(width) {
+  switch(width)
+  {
     case I2S_PARALLEL_WIDTH_8:
       // IS21 supports space saving single byte 8 bit parallel access
-      if(port == I2S_NUM_1) {
+      if(port == I2S_NUM_1)
+      {
         return 1;
       }
     case I2S_PARALLEL_WIDTH_16:
@@ -47,3 +70,7 @@ static inline int i2s_parallel_get_memory_width(i2s_port_t port, i2s_parallel_sa
       return -ESP_ERR_INVALID_ARG;
   }
 }
+
+#ifdef __cplusplus
+}
+#endif
